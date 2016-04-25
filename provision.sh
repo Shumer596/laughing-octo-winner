9e35dd47cfc999b54c232575cdd47115f7c3ff08
shumer <shumerok@gmail.com>
2016-04-25 19:51:33 +0300

    install

---
+++ provision.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get update -q
+
+echo "Install Git"
+sudo apt-get install git -y > /dev/null 2>&1
+
+echo "Install htop"
+sudo apt-get install htop -y > /dev/null 2>&1
+
+echo "Install nginx, php5-fpm"
+sudo apt-get install  nginx php5-fpm -y > /dev/null 2>&1
+
+echo "Preparing MySQL"
+sudo apt-get install debconf-utils -y > /dev/null 2>&1
+debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+
+echo "Install mysql-server" 
+sudo apt-get install mysql-server -y > /dev/null 2>&1
+sudo apt-get install mysql-client -y > /dev/null 2>&1
+
+echo "Install commonly used php packages"
+sudo aptitude install -q -y -f php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xcache > /dev/null 2>&1
+
+sudo rm /etc/nginx/sites-available/default
+sudo touch /etc/nginx/sites-available/default
+
+sudo cat >> /etc/nginx/sites-available/default <<'EOF'
+server {
+  listen   80;
+
+  root /usr/share/nginx/html;
+  index index.php index.html index.htm;
+
+  # Make site accessible from http://localhost/
+  server_name localhost;
+
+  location / {
+    # First attempt to serve request as file, then
+    # as directory, then fall back to index.html
+    try_files $uri $uri/ /index.html;
+  }
+
+  location /doc/ {
+    alias /usr/share/doc/;
+    autoindex on;
+    allow 127.0.0.1;
+    deny all;
+  }
+
+  # redirect server error pages to the static page /50x.html
+  #
+  error_page 500 502 503 504 /50x.html;
+  location = /50x.html {
+    root /usr/share/nginx/html;
+  }
+
+  # pass the PHP scripts to FastCGI server listening on /tmp/php5-fpm.sock
+  #
+  location ~ \.php$ {
+    try_files $uri =404;
+    fastcgi_split_path_info ^(.+\.php)(/.+)$;
+    fastcgi_pass unix:/var/run/php5-fpm.sock;
+    fastcgi_index index.php;
+    include fastcgi_params;
+  }
+
+  # deny access to .htaccess files, if Apache's document root
+  # concurs with nginx's one
+  #
+  location ~ /\.ht {
+    deny all;
+  }
+
+  ### phpMyAdmin ###
+  location /phpmyadmin {
+    root /usr/share/;
+    index index.php index.html index.htm;
+    location ~ ^/phpmyadmin/(.+\.php)$ {
+      client_max_body_size 4M;
+      client_body_buffer_size 128k;
+      try_files $uri =404;
+      root /usr/share/;
+
+      # Point it to the fpm socket;
+      fastcgi_pass unix:/var/run/php5-fpm.sock;
+      fastcgi_index index.php;
+      fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+      include /etc/nginx/fastcgi_params;
+    }
+
+    location ~* ^/phpmyadmin/(.+\.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt)) {
+      root /usr/share/;
+    }
+  }
+  location /phpMyAdmin {
+    rewrite ^/* /phpmyadmin last;
+  }
+  ### phpMyAdmin ###
+}
+EOF
+
+sudo touch /usr/share/nginx/html/info.php
+sudo cat >> /usr/share/nginx/html/info.php <<'EOF'
+<?php phpinfo(); ?>
+EOF
+
+sudo apt-get install  phpmyadmin -y > /dev/null 2>&1
+
+sudo service nginx restart
+
+sudo service php5-fpm restart
+
+echo "Install Composer"
+
+php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer > /dev/null 2>&1
+
+echo "===================="
+echo "====WELL DONE======="
+echo "===================="
